f3ed34a8162b29569ce23817ecec49417388deee
BCAPATHSHALA <manojkumargfg.1@gmail.com>
2025-02-10 11:27:06 +0530

    add loader while fetching the all wireframes

--- app/(routes)/designs/page.tsx
+++ app/(routes)/designs/page.tsx
@@ -5,6 +5,7 @@ import axios from "axios";
 import React, { useEffect, useState } from "react";
 import DesignCard from "./_components/DesignCard";
 import { RECORD } from "@/app/view-code/[uid]/page";
+import { Loader2 } from "lucide-react";
 
 const page = () => {
   //   const { user } = useAuthContext(); // todo: get user after login
@@ -35,11 +36,17 @@ const page = () => {
     <div>
       <h2 className="text-3xl font-bold">Wireframe & Codes</h2>
 
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
-        {wireframesList?.map((wireframe: RECORD, index) => (
-          <DesignCard key={index} wireframe={wireframe} />
-        ))}
-      </div>
+      {loading ? (
+        <div className="flex justify-center items-center h-[80vh]">
+          <Loader2 className="animate-spin w-10 h-10" />
+        </div>
+      ) : (
+        <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+          {wireframesList?.map((wireframe: RECORD, index) => (
+            <DesignCard key={index} wireframe={wireframe} />
+          ))}
+        </div>
+      )}
     </div>
   );
 };
